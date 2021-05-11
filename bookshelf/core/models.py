8661d2ab3e4decd8caf4954666bc3dfe36f387dd
Anand-S23 <anandsroopsingh23@gmail.com>
2021-05-11 19:39:42 -0400

    Switch to express

--- bookshelf/core/models.py
+++ bookshelf/core/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Genre(models.Model):
-    name = models.CharField(max_lenght=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 class Author(models.Model):
-    name = models.CharField(max_lenght=150)
+    name = models.CharField(max_length=150)
     date_of_birth = models.DateField()
     date_of_death = models.DateField()
     bio = models.TextField()
@@ -20,16 +20,23 @@ class Author(models.Model):
         return self.name
 
 class Book(models.Model):
-    name = models.CharField(max_lenght=300)
+    title = models.CharField(max_length=300)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     summary = models.TextField()
     rating = models.FloatField(default=0)
     genre = models.ManyToManyField(Genre)
-    isbn = models.CharField('ISBN', max_lenght=13, unique=True)
+    isbn = models.CharField('ISBN', max_length=13, unique=True)
     book_cover = models.ImageField(verbose_name='Book Cover',
                                    blank=True, 
                                    upload_to='books',
                                    default='/books/default.jpg')
+    
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+    # status = models.
+    pass
 
 class Review(models.Model):
     pass
